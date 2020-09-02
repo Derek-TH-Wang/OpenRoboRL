fc3f1f5582b820ab1b16716708a3e4b1f7c34696
Derek_TH_Wang <416338223@qq.com>
2020-09-02 15:57:16 +0800

    add stable-baselines
    Change-Id: I87f940a80fc2d26a5f1cf1cd56b45a7d201dc07f

--- OpenRoboRL/stable_baselines/common/runners.py
+++ OpenRoboRL/stable_baselines/common/runners.py
@@ -1,22 +1,59 @@
-# coding=utf-8
-# Copyright 2020 The Google Research Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+from abc import ABC, abstractmethod
+import typing
+from typing import Union, Optional, Any
 
 import gym
 import numpy as np
 
+from stable_baselines.common.callbacks import BaseCallback
 from stable_baselines.common.vec_env import VecEnv
+
+if typing.TYPE_CHECKING:
+    from stable_baselines.common.base_class import BaseRLModel  # pytype: disable=pyi-error
+
+
+class AbstractEnvRunner(ABC):
+    def __init__(self, *, env: Union[gym.Env, VecEnv], model: 'BaseRLModel', n_steps: int):
+        """
+        Collect experience by running `n_steps` in the environment.
+        Note: if this is a `VecEnv`, the total number of steps will
+        be `n_steps * n_envs`.
+
+        :param env: (Union[gym.Env, VecEnv]) The environment to learn from
+        :param model: (BaseRLModel) The model to learn
+        :param n_steps: (int) The number of steps to run for each environment
+        """
+        self.env = env
+        self.model = model
+        n_envs = env.num_envs
+        self.batch_ob_shape = (n_envs * n_steps,) + env.observation_space.shape
+        self.obs = np.zeros((n_envs,) + env.observation_space.shape, dtype=env.observation_space.dtype.name)
+        self.obs[:] = env.reset()
+        self.n_steps = n_steps
+        self.states = model.initial_state
+        self.dones = [False for _ in range(n_envs)]
+        self.callback = None  # type: Optional[BaseCallback]
+        self.continue_training = True
+        self.n_envs = n_envs
+
+    def run(self, callback: Optional[BaseCallback] = None) -> Any:
+        """
+        Collect experience.
+
+        :param callback: (Optional[BaseCallback]) The callback that will be called
+            at each environment step.
+        """
+        self.callback = callback
+        self.continue_training = True
+        return self._run()
+
+    @abstractmethod
+    def _run(self) -> Any:
+        """
+        This method must be overwritten by child class.
+        """
+        raise NotImplementedError
+
 
 def traj_segment_generator(policy, env, horizon, reward_giver=None, gail=False, callback=None):
     """
@@ -36,6 +73,7 @@ def traj_segment_generator(policy, env, horizon, reward_giver=None, gail=False, 
         - episode_starts: (numpy bool)
             True if first timestep of an episode, used for GAE
         - actions: (np.ndarray) actions
+        - nextvpred: (numpy float) next action logits
         - ep_rets: (float) cumulated current episode reward
         - ep_lens: (int) the length of the current episode
         - ep_true_rets: (float) the real environment reward
@@ -44,55 +82,41 @@ def traj_segment_generator(policy, env, horizon, reward_giver=None, gail=False, 
     """
     # Check when using GAIL
     assert not (gail and reward_giver is None), "You must pass a reward giver when using GAIL"
-    
-    num_robot = env.num_robot
-    if horizon % num_robot != 0:
-        horizon += (num_robot - horizon % num_robot)
 
     # Initialize state variables
     step = 0
+    action = env.action_space.sample()  # not used, just so we have the datatype
     observation = env.reset()
-    action = [env.action_space.sample() for _ in range(num_robot)] # not used, just so we have the datatype
-    vpred = [0 for _ in range(num_robot)]
-    info = [0 for _ in range(num_robot)]
-    reward = [0 for _ in range(num_robot)]
-    done = [False for _ in range(num_robot)]
-    state = [policy.initial_state for _ in range(num_robot)]
 
     cur_ep_ret = 0  # return in current episode
     current_it_len = 0  # len of current iteration
-    current_ep_len = 0 # len of current episode
+    current_ep_len = 0  # len of current episode
     cur_ep_true_ret = 0
     ep_true_rets = []
     ep_rets = []  # returns of completed episodes in this segment
     ep_lens = []  # Episode lengths
 
     # Initialize history arrays
-    observations = np.array([observation[0] for _ in range(horizon)])
+    observations = np.array([observation for _ in range(horizon)])
     true_rewards = np.zeros(horizon, 'float32')
     rewards = np.zeros(horizon, 'float32')
     vpreds = np.zeros(horizon, 'float32')
-    nextvpreds = np.zeros(horizon, 'float32')
     episode_starts = np.zeros(horizon, 'bool')
     dones = np.zeros(horizon, 'bool')
-    actions = np.array([action[0] for _ in range(horizon)])
-    episode_start = [True for _ in range(num_robot)]  # marks if we're on first timestep of an episode
+    actions = np.array([action for _ in range(horizon)])
+    states = policy.initial_state
+    episode_start = True  # marks if we're on first timestep of an episode
+    done = False
 
     callback.on_rollout_start()
 
     while True:
-        for j in range(num_robot):
-            observation[j] = observation[j].reshape(-1, *observation[j].shape)
-            act, vpred[j], state[j], info[j] = policy.step(observation[j], state[j], done[j])
-            action[j] = act
+        action, vpred, states, _ = policy.step(observation.reshape(-1, *observation.shape), states, done)
         # Slight weirdness here because we need value function at time T
         # before returning segment [0, T-1] so we get the correct
         # terminal value
         if step > 0 and step % horizon == 0:
-            last_vpred = 0.0
-            for j in range(num_robot):
-                nextvpreds[i+j] = last_vpred
-
+            callback.update_locals(locals())
             callback.on_rollout_end()
             yield {
                     "observations": observations,
@@ -101,16 +125,15 @@ def traj_segment_generator(policy, env, horizon, reward_giver=None, gail=False, 
                     "episode_starts": episode_starts,
                     "true_rewards": true_rewards,
                     "vpred": vpreds,
-                    "nextvpreds": nextvpreds,
                     "actions": actions,
+                    "nextvpred": vpred[0] * (1 - episode_start),
                     "ep_rets": ep_rets,
                     "ep_lens": ep_lens,
                     "ep_true_rets": ep_true_rets,
                     "total_timestep": current_it_len,
                     'continue_training': True
             }
-            for j in range(num_robot):
-                _, vpred[j], _, info[j] = policy.step(observation[j])
+            _, vpred, _, _ = policy.step(observation.reshape(-1, *observation.shape))
             # Be careful!!! if you change the downstream algorithm to aggregate
             # several of these batches, then be sure to do a deepcopy
             ep_rets = []
@@ -119,33 +142,26 @@ def traj_segment_generator(policy, env, horizon, reward_giver=None, gail=False, 
             # Reset current iteration length
             current_it_len = 0
             callback.on_rollout_start()
-
         i = step % horizon
-        for j in range(num_robot):
-            observations[i+j] = observation[j]
-            vpreds[i+j] = vpred[j][0]
-            actions[i+j] = action[j][0]
-            episode_starts[i+j] = episode_start[j]
+        observations[i] = observation
+        vpreds[i] = vpred[0]
+        actions[i] = action[0]
+        episode_starts[i] = episode_start
 
-            if (not episode_start[j]) and (i > 0):
-                    nextvpreds[i-1+j] = vpred[j][0]
-
-        clipped_action = [0 for _ in range(num_robot)]
+        clipped_action = action
         # Clip the actions to avoid out of bound error
         if isinstance(env.action_space, gym.spaces.Box):
-            for j in range(num_robot):
-                clipped_action[j] = np.clip(action[j], env.action_space.low, env.action_space.high)[0]
+            clipped_action = np.clip(action, env.action_space.low, env.action_space.high)
 
-        # if gail:
-        #     reward = reward_giver.get_reward(observation, clipped_action[0])
-        #     observation, true_reward, done, info = env.step(clipped_action[0])
-        # else:
-        #     observation, reward, done, info = env.step(clipped_action[0])
-        #     true_reward = reward
-        observation, reward, done, info = env.step(clipped_action)
-        true_reward = reward
+        if gail:
+            reward = reward_giver.get_reward(observation, clipped_action[0])
+            observation, true_reward, done, info = env.step(clipped_action[0])
+        else:
+            observation, reward, done, info = env.step(clipped_action[0])
+            true_reward = reward
 
         if callback is not None:
+            callback.update_locals(locals())
             if callback.on_step() is False:
                 # We have to return everything so pytype does not complain
                 yield {
@@ -155,8 +171,8 @@ def traj_segment_generator(policy, env, horizon, reward_giver=None, gail=False, 
                     "episode_starts": episode_starts,
                     "true_rewards": true_rewards,
                     "vpred": vpreds,
-                    "nextvpreds": nextvpreds,
                     "actions": actions,
+                    "nextvpred": vpred[0] * (1 - episode_start),
                     "ep_rets": ep_rets,
                     "ep_lens": ep_lens,
                     "ep_true_rets": ep_true_rets,
@@ -165,38 +181,29 @@ def traj_segment_generator(policy, env, horizon, reward_giver=None, gail=False, 
                     }
                 return
 
-        for j in range(num_robot):
-            rewards[i+j] = reward[0]
-            true_rewards[i+j] = true_reward[0]
-            dones[i+j] = done[j]
-            episode_start[j] = done[j]
+        rewards[i] = reward
+        true_rewards[i] = true_reward
+        dones[i] = done
+        episode_start = done
 
-            cur_ep_ret += reward[j]
-            cur_ep_true_ret += true_reward[j]
-            current_it_len += 1
-            current_ep_len += 1
-        
-        if all(done):
-            last_vpred = 0.0
-            for j in range(num_robot):
-                nextvpreds[i+j] = last_vpred
+        cur_ep_ret += reward
+        cur_ep_true_ret += true_reward
+        current_it_len += 1
+        current_ep_len += 1
+        if done:
+            # Retrieve unnormalized reward if using Monitor wrapper
+            maybe_ep_info = info.get('episode')
+            if maybe_ep_info is not None:
+                if not gail:
+                    cur_ep_ret = maybe_ep_info['r']
+                cur_ep_true_ret = maybe_ep_info['r']
 
-                # Retrieve unnormalized reward if using Monitor wrapper
-                maybe_ep_info = info[j].get('episode')
-                if maybe_ep_info is not None:
-                    if not gail:
-                        cur_ep_ret = maybe_ep_info['r']
-                    cur_ep_true_ret = maybe_ep_info['r']
-
-                ep_rets.append(cur_ep_ret)
-                ep_true_rets.append(cur_ep_true_ret)
-                ep_lens.append(current_ep_len)
-                cur_ep_ret = 0
-                cur_ep_true_ret = 0
-                current_ep_len = 0
-
+            ep_rets.append(cur_ep_ret)
+            ep_true_rets.append(cur_ep_true_ret)
+            ep_lens.append(current_ep_len)
+            cur_ep_ret = 0
+            cur_ep_true_ret = 0
+            current_ep_len = 0
             if not isinstance(env, VecEnv):
                 observation = env.reset()
-
-        step += num_robot
-    return
+        step += 1
